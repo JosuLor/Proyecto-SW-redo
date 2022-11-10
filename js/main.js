09c0dcad9bb47d1921dcb4cb33c22aa0e418bdf3
Josu Lorenzo <lorenzo.josu@gmail.com>
2022-11-10 23:13:04 +0100

    hgiihj

--- js/main.js
+++ js/main.js
@@ -3,15 +3,16 @@ import { fetchJSON } from "./loaders.js";
 
 // ejercicio 6.1
 function differenceInDays(date1) {
-    // YOUR CODE HERE
+  let difference = new Date().getTime() - date1.getTime();
+  let days = Math.floor(difference / (1000 * 3600 * 24));
+  return days + 1; //ese +1 es para que cuente el dia actual
 }
 
 let difference_In_Days = differenceInDays(new Date("08-18-2022"));
 
 window.onload = function () {
-  document.getElementById(
-    "gamenumber"
-  ).innerText = difference_In_Days.toString();
+  document.getElementById("gamenumber").innerText =
+    difference_In_Days.toString();
   document.getElementById("back-icon").innerHTML = folder + leftArrow;
 };
 
@@ -20,24 +21,23 @@ let game = {
   guesses: [],
   solution: {},
   players: [],
-  leagues: []
+  leagues: [],
 };
 
 // ejercicio 6.3
 function getSolution(players, solutionArray, difference_In_Days) {
- 
-    // YOUR CODE HERE 
+  let sol = solutionArray[(difference_In_Days%solutionArray.length) - 1];
+  let player = players.find((p) => p.id == sol.id);
+  return player;
 }
 
-Promise.all([fetchJSON("fullplayers"), fetchJSON("solution")]).then(
+Promise.all([fetchJSON("../json/fullplayers.json"), fetchJSON("../json/solution.json")]).then(
   (values) => {
-
     let solution;
-    
     [game.players, solution] = values;
 
     game.solution = getSolution(game.players, solution, difference_In_Days);
-    
+
     console.log(game.solution);
 
     document.getElementById(
@@ -45,6 +45,7 @@ Promise.all([fetchJSON("fullplayers"), fetchJSON("solution")]).then(
     ).src = `https://playfootball.games/media/players/${
       game.solution.id % 32
     }/${game.solution.id}.png`;
-  
   }
 );
+
+//sfghjksghfjkglhkkdstaytkflg
